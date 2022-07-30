73fa919387e78e72fbc0b174a10302f6938c187f
hangze <276095361@qq.com>
2022-07-31 00:25:41 +0800

    fea:完成人物、商品、商品每日价格的增删改查

---
+++ src/main/java/com/longjiang/jiangxia/demo1/daoentity/DealRecord.java
@@ -0,0 +1,120 @@
+package com.longjiang.jiangxia.demo1.daoentity;
+
+import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.time.LocalDate;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hang
+ * @since 2022-07-30
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="DealRecord对象", description="")
+public class DealRecord implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "日期")
+    private LocalDate date;
+
+    @ApiModelProperty(value = "商品Id")
+    private Integer goodId;
+
+    @ApiModelProperty(value = "商品名")
+    private String goodName;
+
+    @ApiModelProperty(value = "类型：0->buy,1->sell")
+    private Integer type;
+
+    @ApiModelProperty(value = "价格")
+    private BigDecimal price;
+
+    @ApiModelProperty(value = "数量")
+    private BigDecimal amount;
+
+    @ApiModelProperty(value = "任务Id")
+    private Integer personId;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(Integer goodId) {
+        this.goodId = goodId;
+    }
+
+    public String getGoodName() {
+        return goodName;
+    }
+
+    public void setGoodName(String goodName) {
+        this.goodName = goodName;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+}
---
+++ src/main/java/com/longjiang/jiangxia/demo1/mapper/DealRecordMapper.java
@@ -0,0 +1,16 @@
+package com.longjiang.jiangxia.demo1.mapper;
+
+import com.longjiang.jiangxia.demo1.daoentity.DealRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author hang
+ * @since 2022-07-30
+ */
+public interface DealRecordMapper extends BaseMapper<DealRecord> {
+
+}
